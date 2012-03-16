265916e8d96a3a03bb44fd779f65c705636e9d1a
Mathieu Lecarme <mathieu@garambrogne.net>
2012-03-16 22:13:56 +0100

    Options.

--- src/redis_protocol.erl
+++ src/redis_protocol.erl
@@ -1,6 +1,6 @@
 -module(redis_protocol).
 %% exported functions
--export([start/1]).
+-export([start/1, start/2]).
 %% cowboy callbacks
 -export([start_link/4]).
 %% private functions
@@ -11,16 +11,23 @@
 -record(connection, {
     socket,
     transport,
-    state = []
+    state,
+    options = []
     }).
 
 %% @doc Start the redis_protocol server.
 start(Port) ->
+    start(Port, []).
+
+%% Options
+%%  timeout, default 30000 (30 seconds) connection timeout
+%%  state, [], default state
+start(Port, Options) ->
     ok = application:start(cowboy),
     {ok, _} = cowboy:start_listener(
         ?MODULE, 10,
         cowboy_tcp_transport, [{port, Port}],
-        ?MODULE, []),
+        ?MODULE, Options),
     ok.
 
 %% @private Spawn a process to handle a new connection.
@@ -29,16 +36,22 @@ start_link(ListenerPid, Socket, Transport, Options) ->
     {ok, Pid}.
 
 %% @private Handle a new connection.
-handle(_ListenterPid, Socket, Transport, _Options) ->
+handle(_ListenterPid, Socket, Transport, Options) ->
     Parser = eredis_parser:init(),
-    read_line(#connection{socket= Socket, transport=Transport}, Parser, <<>>),
+    State = proplists:get_value(state, Options, []), %% FIXME handle when state is a function
+    read_line(#connection{
+        socket = Socket,
+        transport = Transport,
+        options = Options,
+        state = State}, Parser, <<>>),
     Transport:close(Socket).
 
-read_line(#connection{socket=Socket, transport=Transport} = Connection, Parser, Rest) ->
-    ok = Transport:setopts(Socket, [binary, {active, once}]), %% TODO check options
+read_line(#connection{socket=Socket, transport=Transport, options=Options} = Connection, Parser, Rest) ->
+    ok = Transport:setopts(Socket, [binary, {active, once}]),
+    Timeout = proplists:get_value(timeout, Options, 30000),
     Line = receive {tcp, Socket, ILine} ->
         ILine
-    after 30000 ->
+    after Timeout ->
         exit(timeout)
     end,
     case parse(Connection, Parser, <<Rest/binary, Line/binary>>) of
@@ -62,6 +75,7 @@ parse(Connection, State, Data) ->
             io:format("Error ~p~n", [Error]),
             {error, Error}
     end.
+
 
 action(#connection{socket = Socket, transport=Transport, state=State}, Action) ->
     io:format("Action ~p~n", [Action]),
